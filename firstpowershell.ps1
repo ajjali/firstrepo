d789de70b05bd97ee218232379bed0d7da052e85
ajayjali <ajayjali@gmail.com>
2021-03-31 07:00:11 -0600

    Changes in file

--- firstpowershell.ps1
+++ firstpowershell.ps1
@@ -1,2 +1,3 @@
 write-host "Hello this is Ajay"
 Copy-Item "C:\Users\ARYNIT\Desktop\T1\T1toT2.ps1" -Destination "C:\Users\ARYNIT\Desktop\T2"
+write-host "Coping powershell files from T1 to T2 completed"
